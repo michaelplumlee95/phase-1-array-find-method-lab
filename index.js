476b38bccf7d4788dc7cff5d5d0c45c33fb8bd86
Michael Plumlee <michaelplumlee95@gmail.com>
2023-03-20 11:45:48 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+
+function superbowlWin(array) {
+  const isWin = array.find((o) => o.result === "W");
+  if (isWin === undefined) {
+    return isWin;
+  } else return isWin.year;
+}
+
+console.log(superbowlWin(record));
